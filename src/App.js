29a8408936900005e2d90699f4fdabb9eb18b801
Kangkan Lahkar <kangkan.lahkar@samarth.ac.in>
2023-06-16 13:22:11 +0530

    header and footer configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h2>TO be done</h2>
+      <Footer />
     </div>
   );
 }
